62ae7fd34620f60762e0df9c8747ec0cae5250d9
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2023-06-29 11:15:19 +0100

    Added encoding tests

--- source/test/test.ash.cpp
+++ source/test/test.ash.cpp
@@ -4,6 +4,7 @@
 #include "ash.test.integer.h"
 #include "ash.test.calendar.h"
 #include "ash.test.unicode.h"
+#include "ash.test.encoding.h"
 
 
 int main()
@@ -16,6 +17,7 @@ int main()
 		Ash::Test::testInteger,
 		Ash::Test::testCalendar,
 		Ash::Test::testUnicode,
+		Ash::Test::testEncoding
 	);
 
 	Ash::Test::Result result = Ash::Test::Unit::run({ testSuite });
