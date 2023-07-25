cc66db0754b19c0f35206388ddb3b491f0ca542f
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2023-07-25 18:21:10 +0100

    Initial check in

---
+++ source/library/ash.real.h
@@ -0,0 +1,184 @@
+#pragma once
+
+#include <cmath>
+#include <limits>
+#include "ash.integer.bit.h"
+
+
+namespace Ash
+{
+	template <typename FLOAT_TYPE>
+	class Real
+	{
+	public:
+		using Type = FLOAT_TYPE;
+
+		using Exponent = int;
+
+		using Size = int;
+
+		static constexpr Exponent minExponent = std::numeric_limits<Type>::min_exponent - 1;
+
+		static constexpr Exponent maxExponent = std::numeric_limits<Type>::max_exponent - 1;
+
+		static constexpr Exponent minSubNormalExponent = std::numeric_limits<Type>::min_exponent - std::numeric_limits<Type>::digits;
+
+		static constexpr Size fractionSize = std::numeric_limits<Type>::digits - 1;
+
+		static constexpr Size exponentSize = Ash::Integer::getBitSize<unsigned int>(maxExponent - minExponent);
+
+		static constexpr Real epsilon = std::numeric_limits<Type>::epsilon();
+
+		static constexpr Real infinity = std::numeric_limits<Type>::infinity();
+
+		static constexpr Real invalid = std::numeric_limits<Type>::quiet_NaN();
+
+		static constexpr Real min = std::numeric_limits<Type>::min();
+
+		static constexpr Real max = std::numeric_limits<Type>::max();
+
+		static constexpr Real minSubNormal = std::numeric_limits<Type>::denorm_min();
+
+		static constexpr Real pi = 3.1415926535897932385L;
+
+		constexpr Real() : m_Value(0.0) {}
+
+		template <typename VALUE>
+		constexpr Real(VALUE value) : m_Value(value) {}
+
+		template <typename VALUE>
+		constexpr Real(VALUE value, Exponent exponent) : m_Value(std::ldexp(Type(value), exponent)) {}
+
+		constexpr operator Type () const { return m_Value; }
+
+		constexpr bool isValid() const { return !std::isnan(m_Value); }
+
+		constexpr bool isInfinite() const { return std::isinf(m_Value); }
+
+		constexpr bool isNegative() const { return std::signbit(m_Value); }
+
+		constexpr bool isPositive() const { return !isNegative(); }
+
+		constexpr Real negate() const { return -m_Value; }
+
+		constexpr Real getAbsolute() const { return std::fabs(m_Value); }
+
+		enum MatchType
+		{
+			MatchAbsolute,
+			MatchRelative
+		};
+
+		constexpr Real match(Real value, MatchType matchType = MatchAbsolute) const
+		{
+			return (matchType == MatchAbsolute) ? matchAbsolute(value) : matchRelative(value);
+		}
+
+		constexpr bool isEqual(Real value, MatchType matchType = MatchAbsolute, Real tolerance = 1.0) const
+		{
+			Real error = match(value, matchType);
+			
+			return error.isValid() && (error <= tolerance);
+		}
+
+		constexpr Real squareRoot() const
+		{
+			return std::sqrt(m_Value);
+		}
+
+	protected:
+		constexpr Real matchAbsolute(Real value) const
+		{
+			if (m_Value == value)
+			{
+				return 0.0;
+			}
+			else if (!isValid() || !value.isValid() || isInfinite() || value.isInfinite())
+			{
+				return invalid;
+			}
+			else if (m_Value == 0)
+			{
+				return (value.getAbsolute() < min) ? Real(value.getAbsolute() / minSubNormal / 2) : invalid;
+			}
+			else if (value == 0)
+			{
+				return (getAbsolute() < min) ? Real(getAbsolute() / minSubNormal / 2) : invalid;
+			}
+			else if (isPositive() ? value.isNegative() : value.isPositive())
+			{
+				return invalid;
+			}
+			else
+			{
+				Exponent thisExponent;
+				Exponent valueExponent;
+				Exponent exponent;
+
+				Real thisFraction = std::frexp(m_Value, &thisExponent);
+				Real valueFraction = std::frexp(value, &valueExponent);
+				Real fraction;
+
+				if (thisExponent == valueExponent + 1)
+				{
+					thisFraction = thisFraction * 2;
+					thisExponent = thisExponent - 1;
+				}
+				else if (thisExponent + 1 == valueExponent)
+				{
+					valueFraction = valueFraction * 2;
+				}
+				else if (thisExponent != valueExponent)
+				{
+					return invalid;
+				}
+
+				fraction = thisFraction - valueFraction;
+				exponent = (thisExponent <= minExponent) ? fractionSize - minExponent + thisExponent - 1 : fractionSize;
+
+				return { fraction.getAbsolute(), exponent };
+			}
+		}
+
+		constexpr Real matchRelative(Real value) const
+		{
+			if (m_Value == value)
+			{
+				return 0.0;
+			}
+			else if (!isValid() || !value.isValid() || isInfinite() || value.isInfinite())
+			{
+				return invalid;
+			}
+			else if (m_Value == 0)
+			{
+				return value.getAbsolute() / epsilon / 2;
+			}
+			else if (value == 0)
+			{
+				return getAbsolute() / epsilon / 2;
+			}
+			else if (isPositive() ? value.isNegative() : value.isPositive())
+			{
+				return invalid;
+			}
+			else if (getAbsolute() > value.getAbsolute())
+			{
+				return (Real(1.0) - value / m_Value) / epsilon;
+			}
+			else
+			{
+				return (Real(1.0) - m_Value / value) / epsilon;
+			}
+		}
+
+	private:
+		Type m_Value;
+	};
+
+	using Float = Real<float>;
+
+	using Double = Real<double>;
+
+	using LongDouble = Real<long double>;
+}
