11413e0fec11d2a6d2fc9c0b0359fb982d5c3bb6
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2023-08-08 15:26:52 +0100

    Added Vector2D test unit

--- source/test/ash.test.geometry.h
+++ source/test/ash.test.geometry.h
@@ -2,6 +2,7 @@
 
 #include "ash.test.geometry.angle.h"
 #include "ash.test.geometry.coordinate2d.h"
+#include "ash.test.geometry.vector2d.h"
 
 
 namespace Ash
@@ -12,7 +13,8 @@ namespace Ash
 		(
 			testGeometry,
 			Ash::Test::testAngle,
-			Ash::Test::testCoordinate2D
+			Ash::Test::testCoordinate2D,
+			Ash::Test::testVector2D
 		);
 	}
 }
