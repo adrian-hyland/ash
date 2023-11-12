dcc6ec8043b2d67e39ca700ccc5e3caf1f3efcba
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2023-11-12 12:37:02 +0000

    Added FileSystem namespace

---
+++ source/library/ash.system.linux.filesystem.h
@@ -0,0 +1,321 @@
+#pragma once
+
+#include <cstdio>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/file.h>
+#include <sys/stat.h>
+#include "ash.utf8.h"
+#include "ash.wide.h"
+
+
+namespace Ash
+{
+	namespace System
+	{
+		namespace Linux
+		{
+			namespace FileSystem
+			{
+				class File
+				{
+				public:
+					enum Access
+					{
+						SharedRead,
+						ExclusiveRead,
+						ExclusiveWrite,
+						ExclusiveReadWrite
+					};
+
+					enum Create
+					{
+						Always,
+						New,
+						Never
+					};
+
+					using Handle = FILE *;
+
+					using Position = off64_t;
+
+					inline File() : m_Handle(nullptr) {}
+
+					inline File(File &&file) noexcept : m_Handle(file.m_Handle) { file.m_Handle = nullptr; }
+
+					virtual inline ~File()
+					{
+						close();
+					}
+
+					inline File &operator = (File &&file) noexcept
+					{
+						if (this != &file)
+						{
+							close();
+							
+							m_Handle = file.m_Handle;
+							file.m_Handle = nullptr;
+						}
+
+						return *this;
+					}
+
+					inline operator Handle *() { return (m_Handle != nullptr) ? &m_Handle : nullptr; }
+
+					inline bool isOpen() const { return m_Handle != nullptr; }
+
+					template
+					<
+						typename ALLOCATION,
+						typename ENCODING,
+						typename = Ash::Type::IsClass<ALLOCATION, Ash::Memory::Generic::Allocation>,
+						typename = Ash::Type::IsClass<ENCODING, Ash::Generic::Encoding>
+					>
+					inline bool open(const Ash::String::Value<ALLOCATION, ENCODING> &fileName, Create create, Access access)
+					{
+						return open(Path(fileName), create, access);
+					}
+
+					inline bool open(const Ash::Encoding::CodeUnit8 *fileName, Create create, Access access)
+					{
+						return open(Path(fileName), create, access);
+					}
+
+					inline bool open(const Ash::Encoding::CodeUnitWide *fileName, Create create, Access access)
+					{
+						return open(Path(fileName), create, access);
+					}
+
+					inline void close()
+					{
+						if (m_Handle != nullptr)
+						{
+							::fclose(m_Handle);
+							m_Handle = nullptr;
+						}
+					}
+
+					inline bool movePositionToStart(Position offset = 0)
+					{
+						return (m_Handle != nullptr) && (::fseeko64(m_Handle, offset, SEEK_SET) == 0);
+					}
+
+					inline bool movePositionToEnd(Position offset = 0)
+					{
+						return (m_Handle != nullptr) && (::fseeko64(m_Handle, offset, SEEK_END) == 0);
+					}
+
+					inline bool movePosition(Position offset)
+					{
+						return (m_Handle != nullptr) && (::fseeko64(m_Handle, offset, SEEK_CUR) == 0);
+					}
+
+					inline bool getPosition(Position &position)
+					{
+						if (m_Handle == nullptr)
+						{
+							return false;
+						}
+
+						position = ::ftello64(m_Handle);
+
+						return position >= 0;
+					}
+
+					template
+					<
+						typename TYPE,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool read(TYPE &byte)
+					{
+						return (m_Handle != nullptr) && (::fread(&byte, sizeof(byte), 1, m_Handle) == 1);
+					}
+
+					template
+					<
+						typename TYPE,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool write(TYPE byte)
+					{
+						return (m_Handle != nullptr) && (::fwrite(&byte, sizeof(byte), 1, m_Handle) == 1);
+					}
+
+					template
+					<
+						typename TYPE,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool append(TYPE byte)
+					{
+						return movePositionToEnd() && write(byte);
+					}
+
+					template
+					<
+						typename ALLOCATION,
+						typename TYPE,
+						typename = Ash::Type::IsClass<ALLOCATION, Ash::Memory::Generic::Allocation>,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool read(Ash::Memory::Value<ALLOCATION, TYPE> &content, size_t &length)
+					{
+						size_t offset = content.getLength();
+
+						if ((m_Handle == nullptr) || !content.increaseLength(length))
+						{
+							return false;
+						}
+
+						Ash::Memory::Area<TYPE> memoryArea = content.getArea(offset, length);
+
+						length = ::fread(memoryArea.at(0), sizeof(TYPE), memoryArea.getLength(), m_Handle);
+						
+						if (length < memoryArea.getLength())
+						{
+							if (!::feof(m_Handle))
+							{
+								length = 0;
+								content.setLength(offset);
+								return false;
+							}
+						}
+
+						return content.setLength(offset + length);
+					}
+
+					template
+					<
+						typename ALLOCATION,
+						typename TYPE,
+						typename = Ash::Type::IsClass<ALLOCATION, Ash::Memory::Generic::Allocation>,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool write(const Ash::Memory::Value<ALLOCATION, TYPE> &content)
+					{
+						return (m_Handle != nullptr) && (::fwrite(content.at(0), sizeof(TYPE), content.getLength(), m_Handle) == content.getLength());
+					}
+
+					template
+					<
+						typename ALLOCATION,
+						typename TYPE,
+						typename = Ash::Type::IsClass<ALLOCATION, Ash::Memory::Generic::Allocation>,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool append(const Ash::Memory::Value<ALLOCATION, TYPE> &content)
+					{
+						return movePositionToEnd() && write(content);
+					}
+
+				protected:
+					using PathEncoding = Ash::Encoding::Utf8;
+
+					class Path : public Ash::String::ArrayBuffer<PathEncoding, 256, 50, 32>
+					{
+					public:
+						using String = Ash::String::ArrayBuffer<PathEncoding, 256, 50, 32>;
+
+						constexpr Path() : String() { normalise(); }
+
+						constexpr Path(const Ash::Encoding::CodeUnit8 *value) : String()
+						{
+							Ash::Utf8::View(value).convertTo(*this, '_');
+							normalise();
+						}
+
+						constexpr Path(const Ash::Encoding::CodeUnitWide *value) : String()
+						{
+							Ash::Wide::View(value).convertTo(*this, '_');
+							normalise();
+						}
+
+						template
+						<
+							typename ALLOCATION,
+							typename ENCODING,
+							typename = Ash::Type::IsClass<ALLOCATION, Ash::Memory::Generic::Allocation>,
+							typename = Ash::Type::IsClass<ENCODING, Ash::Generic::Encoding>
+						>
+						constexpr Path(const Ash::String::Value<ALLOCATION, ENCODING> &value) : String()
+						{
+							value.convertTo(*this, '_');
+							normalise();
+						}
+
+						inline operator const char * () const { return reinterpret_cast<const char *>(String::at(0)); }
+
+					protected:
+						constexpr void normalise()
+						{
+							String::append('\0');
+						}
+					};
+
+					static constexpr int getFlags(Create create, Access access)
+					{
+						int flags = 0;
+
+						if (create == Always)
+						{
+							flags = flags + O_CREAT + O_TRUNC;
+						}
+						else if (create == New)
+						{
+							flags = flags + O_CREAT;
+						}
+
+						if ((access == SharedRead) || (access == ExclusiveRead))
+						{
+							flags = flags + O_RDONLY;
+						}
+						else if (access == ExclusiveWrite)
+						{
+							flags = flags + O_WRONLY;
+						}
+						else // (access == ExclusiveReadWrite)
+						{
+							flags = flags + O_RDWR;
+						}
+
+						return flags;
+					}
+
+					inline bool open(const Path &path, Create create, Access access)
+					{
+						close();
+
+						int file = ::open(path, getFlags(create, access), S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+						if (file < 0)
+						{
+							return false;
+						}
+
+						if ((access != SharedRead) && (::flock(file, LOCK_EX | LOCK_NB) != 0))
+						{
+							::close(file);
+							return false;
+						}
+
+						m_Handle = ::fdopen(file, ((access == SharedRead) || (access == ExclusiveRead)) ? "rb" : "rb+");
+						if (m_Handle == nullptr)
+						{
+							::close(file);
+							return false;
+						}
+
+						return true;
+					}
+
+				private:
+					Handle m_Handle;
+
+					File(const File &file) = delete;
+					File &operator = (const File &file) = delete;
+				};
+			}
+		}
+	}
+}
---
+++ source/library/ash.system.windows.filesystem.h
@@ -0,0 +1,325 @@
+#pragma once
+
+#include <windows.h>
+#include "ash.utf8.h"
+#include "ash.wide.h"
+
+
+namespace Ash
+{
+	namespace System
+	{
+		namespace Windows
+		{
+			namespace FileSystem
+			{
+				class File
+				{
+				public:
+					enum Access
+					{
+						SharedRead,
+						ExclusiveRead,
+						ExclusiveWrite,
+						ExclusiveReadWrite
+					};
+
+					enum Create
+					{
+						Always,
+						New,
+						Never
+					};
+
+					using Handle = HANDLE;
+
+					using Position = int64_t;
+
+					inline File() : m_Handle(INVALID_HANDLE_VALUE) {}
+
+					inline File(File &&file) noexcept : m_Handle(file.m_Handle) { file.m_Handle = INVALID_HANDLE_VALUE; }
+
+					virtual inline ~File()
+					{
+						close();
+					}
+
+					inline File &operator = (File &&file) noexcept
+					{
+						if (this != &file)
+						{
+							close();
+							
+							m_Handle = file.m_Handle;
+							file.m_Handle = INVALID_HANDLE_VALUE;
+						}
+
+						return *this;
+					}
+
+					inline operator Handle *() { return (m_Handle != INVALID_HANDLE_VALUE) ? &m_Handle : nullptr; }
+
+					inline bool isOpen() const { return m_Handle != INVALID_HANDLE_VALUE; }
+
+					template
+					<
+						typename ALLOCATION,
+						typename ENCODING,
+						typename = Ash::Type::IsClass<ALLOCATION, Ash::Memory::Generic::Allocation>,
+						typename = Ash::Type::IsClass<ENCODING, Ash::Generic::Encoding>
+					>
+					inline bool open(const Ash::String::Value<ALLOCATION, ENCODING> &fileName, Create create, Access access)
+					{
+						return open(Path(fileName), create, access);
+					}
+
+					inline bool open(const Ash::Encoding::CodeUnit8 *fileName, Create create, Access access)
+					{
+						return open(Path(fileName), create, access);
+					}
+
+					inline bool open(const Ash::Encoding::CodeUnitWide *fileName, Create create, Access access)
+					{
+						return open(Path(fileName), create, access);
+					}
+
+					inline void close()
+					{
+						if (m_Handle != INVALID_HANDLE_VALUE)
+						{
+							::CloseHandle(m_Handle);
+							m_Handle = INVALID_HANDLE_VALUE;
+						}
+					}
+
+					inline bool movePositionToStart(Position offset = 0)
+					{
+						LARGE_INTEGER currentPosition = {};
+						currentPosition.QuadPart = offset;
+
+						return ::SetFilePointerEx(m_Handle, currentPosition, nullptr, FILE_BEGIN) != 0;
+					}
+
+					inline bool movePositionToEnd(Position offset = 0)
+					{
+						LARGE_INTEGER currentPosition = {};
+						currentPosition.QuadPart = offset;
+
+						return ::SetFilePointerEx(m_Handle, currentPosition, nullptr, FILE_END) != 0;
+					}
+
+					inline bool movePosition(Position offset)
+					{
+						LARGE_INTEGER currentPosition = {};
+						currentPosition.QuadPart = offset;
+
+						return ::SetFilePointerEx(m_Handle, currentPosition, nullptr, FILE_CURRENT) != 0;
+					}
+
+					inline bool getPosition(Position &position)
+					{
+						LARGE_INTEGER currentPosition = {};
+						LARGE_INTEGER newPosition = {};
+
+						if (::SetFilePointerEx(m_Handle, currentPosition, &newPosition, FILE_CURRENT) == 0)
+						{
+							return false;
+						}
+						
+						position = newPosition.QuadPart;
+						return true;
+					}
+
+					template
+					<
+						typename TYPE,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool read(TYPE &byte)
+					{
+						DWORD readLength = 0;
+
+						return ::ReadFile(m_Handle, &byte, sizeof(byte), &readLength, nullptr) && (readLength == sizeof(byte));
+					}
+
+					template
+					<
+						typename TYPE,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool write(TYPE byte)
+					{
+						DWORD writeLength = 0;
+
+						return ::WriteFile(m_Handle, &byte, sizeof(byte), &writeLength, nullptr) && (writeLength == sizeof(byte));
+					}
+
+					template
+					<
+						typename TYPE,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool append(TYPE byte)
+					{
+						return movePositionToEnd() && write(byte);
+					}
+
+					template
+					<
+						typename ALLOCATION,
+						typename TYPE,
+						typename = Ash::Type::IsClass<ALLOCATION, Ash::Memory::Generic::Allocation>,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool read(Ash::Memory::Value<ALLOCATION, TYPE> &content, size_t &length)
+					{
+						size_t offset = content.getLength();
+						DWORD readLength;
+
+						if (!content.increaseLength(length) || !::ReadFile(m_Handle, content.at(offset), length, &readLength, nullptr))
+						{
+							return false;
+						}
+
+						length = readLength;
+
+						return content.setLength(offset + length);
+					}
+
+					template
+					<
+						typename ALLOCATION,
+						typename TYPE,
+						typename = Ash::Type::IsClass<ALLOCATION, Ash::Memory::Generic::Allocation>,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool write(const Ash::Memory::Value<ALLOCATION, TYPE> &content)
+					{
+						DWORD writeLength;
+
+						return ::WriteFile(m_Handle, content.at(0), content.getLength(), &writeLength, nullptr) && (writeLength == content.getLength());
+					}
+
+					template
+					<
+						typename ALLOCATION,
+						typename TYPE,
+						typename = Ash::Type::IsClass<ALLOCATION, Ash::Memory::Generic::Allocation>,
+						typename = Ash::Type::IsByteSizeInteger<TYPE>
+					>
+					inline bool append(const Ash::Memory::Value<ALLOCATION, TYPE> &content)
+					{
+						return movePositionToEnd() && write(content);
+					}
+
+				protected:
+					using PathEncoding = Ash::Encoding::Wide;
+
+					class Path : public Ash::String::ArrayBuffer<PathEncoding, 128, 50, 32>
+					{
+					public:
+						using String = Ash::String::ArrayBuffer<PathEncoding, 128, 50, 32>;
+
+						constexpr Path() : String() { normalise(); }
+
+						constexpr Path(const Ash::Encoding::CodeUnit8 *value) : String()
+						{
+							Ash::Utf8::View(value).convertTo(*this, '_');
+							normalise();
+						}
+
+						constexpr Path(const Ash::Encoding::CodeUnitWide *value) : String()
+						{
+							Ash::Wide::View(value).convertTo(*this, '_');
+							normalise();
+						}
+
+						template
+						<
+							typename ALLOCATION,
+							typename ENCODING,
+							typename = Ash::Type::IsClass<ALLOCATION, Ash::Memory::Generic::Allocation>,
+							typename = Ash::Type::IsClass<ENCODING, Ash::Generic::Encoding>
+						>
+						constexpr Path(const Ash::String::Value<ALLOCATION, ENCODING> &value) : String()
+						{
+							value.convertTo(*this, '_');
+							normalise();
+						}
+
+						constexpr operator const PathEncoding::Code * () const { return String::at(0); }
+
+					protected:
+						constexpr void normalise()
+						{
+							if (String::getLength() != 0)
+							{
+								String::insert(0, Ash::String::View<PathEncoding>(L"\\\\?\\"));
+							}
+							String::append('\0');
+						}
+					};
+
+					static constexpr DWORD getCreateFlags(Create create)
+					{
+						if (create == Always)
+						{
+							return CREATE_ALWAYS;
+						}
+						else if (create == New)
+						{
+							return OPEN_ALWAYS;
+						}
+						else // (create == Never)
+						{
+							return OPEN_EXISTING;
+						}
+					}
+
+					static constexpr DWORD getAccessFlags(Access access)
+					{
+						if ((access == SharedRead) || (access == ExclusiveRead))
+						{
+							return GENERIC_READ;
+						}
+						else if (access == ExclusiveWrite)
+						{
+							return GENERIC_WRITE;
+						}
+						else // (access == ExclusiveReadWrite)
+						{
+							return GENERIC_READ | GENERIC_WRITE;
+						}
+					}
+
+					static constexpr DWORD getShareFlags(Access access)
+					{
+						if (access == SharedRead)
+						{
+							return FILE_SHARE_READ;
+						}
+						else // (access != SharedRead)
+						{
+							return 0;
+						}
+					}
+
+					inline bool open(const Path &path, Create create, Access access)
+					{
+						close();
+
+						m_Handle = ::CreateFileW(path, getAccessFlags(access), getShareFlags(access), nullptr, getCreateFlags(create), FILE_ATTRIBUTE_NORMAL, nullptr);
+
+						return m_Handle != INVALID_HANDLE_VALUE;
+					}
+
+				private:
+					Handle m_Handle;
+
+					File(const File &file) = delete;
+					File &operator = (const File &file) = delete;
+				};
+			}
+		}
+	}
+}
