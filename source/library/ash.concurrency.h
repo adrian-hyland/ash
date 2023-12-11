9a2b747b7f341077c59713e7d650bdb88968b4e0
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2023-12-11 17:53:56 +0000

    Declaring the core concurrency classes in the Concurrency namespace (rather than using this as a namespace alias to the target sytem concurrency namespace).

--- source/library/ash.concurrency.h
+++ source/library/ash.concurrency.h
@@ -1,22 +1,36 @@
 #pragma once
 
-
 #if defined __WIN32__
-
 #include "ash.system.windows.concurrency.h"
-
-namespace Ash
-{
-	namespace Concurrency = System::Windows::Concurrency;
-}
-
 #elif defined __linux__
-
 #include "ash.system.linux.concurrency.h"
+#endif
+
 
 namespace Ash
 {
-	namespace Concurrency = System::Linux::Concurrency;
-}
-
+	namespace Concurrency
+	{
+#if defined __WIN32__
+		using Mutex = Ash::System::Windows::Concurrency::Mutex;
+		using Event = Ash::System::Windows::Concurrency::Event;
+		using Semaphore = Ash::System::Windows::Concurrency::Semaphore;
+		template
+		<
+			typename RUNNABLE,
+			bool     IS_DETACHABLE = false
+		>
+		using Thread = Ash::System::Windows::Concurrency::Thread<RUNNABLE, IS_DETACHABLE>;
+#elif defined __linux__
+		using Mutex = Ash::System::Linux::Concurrency::Mutex;
+		using Event = Ash::System::Linux::Concurrency::Event;
+		using Semaphore = Ash::System::Linux::Concurrency::Semaphore;
+		template
+		<
+			typename RUNNABLE,
+			bool     IS_DETACHABLE = false
+		>
+		using Thread = Ash::System::Linux::Concurrency::Thread<RUNNABLE, IS_DETACHABLE>;
 #endif
+	}
+}
