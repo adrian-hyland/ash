5c45d4797e66b3d73ff8a970c10c975c09bc5a22
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2024-04-22 12:56:40 +0100

    Refactored code with additional namespaces:  - the Signed namespace has WithBitSize and WithMaxBitSize structures for deriving a signed integer type with a particular bit size.  - the Unsigned namespace has WithBitSize and WithMaxBitSize structures for deriving an unsigned integer type with a particular bit size.  - the Value namespace has a WithBitSize structure for deriving a signed/unsigned integer type with a particular bit size; and a WithRange structure for deriving a signed/unsigned integer type that can hold a value within a particular range.

--- source/library/ash.integer.value.h
+++ source/library/ash.integer.value.h
@@ -9,92 +9,174 @@ namespace Ash
 {
 	namespace Integer
 	{
-		template
-		<
-			auto     MIN_VALUE,
-			auto     MAX_VALUE,
-			size_t   SCALAR = 1,
-			bool     IS_SIGNED = (MIN_VALUE < 0) || (MAX_VALUE < 0),
-			typename = Ash::Type::IsInteger<decltype(MIN_VALUE)>,
-			typename = Ash::Type::IsInteger<decltype(MAX_VALUE)>
-		>
-		constexpr size_t getValueSize()
+		namespace Signed
 		{
-			std::make_unsigned_t<decltype(MIN_VALUE)> absMinValue = (MIN_VALUE < 0) ? (IS_SIGNED ? -(MIN_VALUE + 1) : 0) : MIN_VALUE;
-			std::make_unsigned_t<decltype(MAX_VALUE)> absMaxValue = (MAX_VALUE < 0) ? (IS_SIGNED ? -(MAX_VALUE + 1) : 0) : MAX_VALUE;
+			template
+			<
+				size_t BIT_SIZE
+			>
+			struct WithBitSize
+			{
+			};
 
-			size_t minByteSize = (Ash::Integer::getBitSize(absMinValue) * SCALAR + (IS_SIGNED ? 8 : 7)) / 8;
-			size_t maxByteSize = (Ash::Integer::getBitSize(absMaxValue) * SCALAR + (IS_SIGNED ? 8 : 7)) / 8;
+			template <> struct WithBitSize<8>
+			{
+				using Type = int8_t;
+			};
 
-			size_t byteSize = std::max(minByteSize, maxByteSize);
-			return (byteSize == 1) ? 8 : 8 << Ash::Integer::getBitSize(byteSize - 1);
+			template <> struct WithBitSize<16>
+			{
+				using Type = int16_t;
+			};
+
+			template <> struct WithBitSize<32>
+			{
+				using Type = int32_t;
+			};
+
+			template <> struct WithBitSize<64>
+			{
+				using Type = int64_t;
+			};
+
+			template
+			<
+				size_t BIT_SIZE_1,
+				size_t BIT_SIZE_2,
+				size_t ...BIT_SIZE_N
+			>
+			struct WithMaxBitSize
+			{
+				using Type = Ash::Integer::Signed::WithMaxBitSize<std::max(BIT_SIZE_1, BIT_SIZE_2), BIT_SIZE_N...>::Type;
+			};
+
+			template
+			<
+				size_t BIT_SIZE_1,
+				size_t BIT_SIZE_2
+			>
+			struct WithMaxBitSize<BIT_SIZE_1, BIT_SIZE_2>
+			{
+				using Type = Ash::Integer::Signed::WithBitSize<std::max(BIT_SIZE_1, BIT_SIZE_2)>::Type;
+			};
 		}
 
-		template
-		<
-			size_t BIT_SIZE
-		>
-		struct ValueProperty
+		namespace Unsigned
 		{
-		};
+			template
+			<
+				size_t BIT_SIZE
+			>
+			struct WithBitSize
+			{
+			};
 
-		template <> struct ValueProperty<8>
+			template <> struct WithBitSize<8>
+			{
+				using Type = uint8_t;
+			};
+
+			template <> struct WithBitSize<16>
+			{
+				using Type = uint16_t;
+			};
+
+			template <> struct WithBitSize<32>
+			{
+				using Type = uint32_t;
+			};
+
+			template <> struct WithBitSize<64>
+			{
+				using Type = uint64_t;
+			};
+
+			template
+			<
+				size_t BIT_SIZE_1,
+				size_t BIT_SIZE_2,
+				size_t ...BIT_SIZE_N
+			>
+			struct WithMaxBitSize
+			{
+				using Type = Ash::Integer::Unsigned::WithMaxBitSize<std::max(BIT_SIZE_1, BIT_SIZE_2), BIT_SIZE_N...>::Type;
+			};
+
+			template
+			<
+				size_t BIT_SIZE_1,
+				size_t BIT_SIZE_2
+			>
+			struct WithMaxBitSize<BIT_SIZE_1, BIT_SIZE_2>
+			{
+				using Type = Ash::Integer::Unsigned::WithBitSize<std::max(BIT_SIZE_1, BIT_SIZE_2)>::Type;
+			};
+		}
+
+		namespace Value
 		{
-			using Type = int8_t;
-		};
+			template
+			<
+				auto     MIN_VALUE,
+				auto     MAX_VALUE,
+				bool     IS_SIGNED = (MIN_VALUE < 0) || (MAX_VALUE < 0),
+				typename = Ash::Type::IsInteger<decltype(MIN_VALUE)>,
+				typename = Ash::Type::IsInteger<decltype(MAX_VALUE)>
+			>
+			constexpr size_t getBitSize()
+			{
+				std::make_unsigned_t<decltype(MIN_VALUE)> absMinValue = (MIN_VALUE < 0) ? (IS_SIGNED ? -(MIN_VALUE + 1) : 0) : MIN_VALUE;
+				std::make_unsigned_t<decltype(MAX_VALUE)> absMaxValue = (MAX_VALUE < 0) ? (IS_SIGNED ? -(MAX_VALUE + 1) : 0) : MAX_VALUE;
 
-		template <> struct ValueProperty<16>
-		{
-			using Type = int16_t;
-		};
+				size_t minByteSize = (Ash::Integer::getBitLength(absMinValue) + (IS_SIGNED ? 8 : 7)) / 8;
+				size_t maxByteSize = (Ash::Integer::getBitLength(absMaxValue) + (IS_SIGNED ? 8 : 7)) / 8;
 
-		template <> struct ValueProperty<32>
-		{
-			using Type = int32_t;
-		};
+				size_t byteSize = std::max(minByteSize, maxByteSize);
+				return (byteSize == 1) ? 8 : 8 << Ash::Integer::getBitLength(byteSize - 1);
+			}
 
-		template <> struct ValueProperty<64>
-		{
-			using Type = int64_t;
-		};
+			template
+			<
+				size_t BIT_SIZE,
+				bool   IS_SIGNED
+			>
+			struct WithBitSize
+			{
+			};
 
-		template
-		<
-			size_t BIT_SIZE,
-			bool   IS_SIGNED
-		>
-		struct SignedProperty
-		{
-		};
+			template
+			<
+				size_t BIT_SIZE
+			>
+			struct WithBitSize<BIT_SIZE, true>
+			{
+				using Type = Ash::Integer::Signed::WithBitSize<BIT_SIZE>::Type;
+			};
 
-		template
-		<
-			size_t BIT_SIZE
-		>
-		struct SignedProperty<BIT_SIZE, true>
-		{
-			using Type = std::make_signed_t<typename ValueProperty<BIT_SIZE>::Type>;
-		};
+			template
+			<
+				size_t BIT_SIZE
+			>
+			struct WithBitSize<BIT_SIZE, false>
+			{
+				using Type = Ash::Integer::Unsigned::WithBitSize<BIT_SIZE>::Type;
+			};
 
-		template
-		<
-			size_t BIT_SIZE
-		>
-		struct SignedProperty<BIT_SIZE, false>
-		{
-			using Type = std::make_unsigned_t<typename ValueProperty<BIT_SIZE>::Type>;
-		};
+			template
+			<
+				auto     MIN_VALUE,
+				auto     MAX_VALUE,
+				bool     IS_SIGNED = (MIN_VALUE < 0) || (MAX_VALUE < 0),
+				typename = Ash::Type::IsInteger<decltype(MIN_VALUE)>,
+				typename = Ash::Type::IsInteger<decltype(MAX_VALUE)>
+			>
+			struct WithRange
+			{
+				using Type = Ash::Integer::Value::WithBitSize<Ash::Integer::Value::getBitSize<MIN_VALUE, MAX_VALUE, IS_SIGNED>(), IS_SIGNED>::Type;
 
-
-		template
-		<
-			auto     MIN_VALUE,
-			auto     MAX_VALUE,
-			size_t   SCALAR = 1,
-			bool     IS_SIGNED = (MIN_VALUE < 0) || (MAX_VALUE < 0),
-			typename = Ash::Type::IsInteger<decltype(MIN_VALUE)>,
-			typename = Ash::Type::IsInteger<decltype(MAX_VALUE)>
-		>
-		using Value = typename SignedProperty<getValueSize<MIN_VALUE, MAX_VALUE, SCALAR, IS_SIGNED>(), IS_SIGNED>::Type;
+				static constexpr Type minValue = MIN_VALUE;
+				static constexpr Type maxValue = MAX_VALUE;
+			};
+		}
 	}
 }
