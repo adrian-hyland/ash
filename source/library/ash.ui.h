3bd409e0787afa1fa346289937155f2e95b1d490
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2025-04-20 16:00:14 +0100

    Moved Ash::UI::Window and Ash::UI::Screen into separate header files.

--- source/library/ash.ui.h
+++ source/library/ash.ui.h
@@ -4,26 +4,5 @@
 #include "ash.ui.generic.h"
 #include "ash.ui.keyboard.h"
 #include "ash.ui.mouse.h"
-
-#if defined PLATFORM_WINDOWS
-#include "ash.system.windows.ui.screen.h"
-#include "ash.system.windows.ui.window.h"
-#elif defined PLATFORM_LINUX
-#include "ash.system.linux.x11.screen.h"
-#include "ash.system.linux.x11.window.h"
-#endif
-
-
-namespace Ash
-{
-	namespace UI
-	{
-#if defined PLATFORM_WINDOWS
-		using Screen = Ash::System::Windows::UI::Screen;
-		using Window = Ash::System::Windows::UI::Window;
-#elif defined PLATFORM_LINUX
-		using Screen = Ash::System::Linux::X11::Screen;
-		using Window = Ash::System::Linux::X11::Window;
-#endif
-	}
-}
+#include "ash.ui.screen.h"
+#include "ash.ui.window.h"
