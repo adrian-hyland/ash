7cc134858a632f51899ec547b873f5753b1931d0
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2025-07-14 14:41:55 +0100

    Renamed 'getBitSize' to just 'bitSize'. Also corrected it to use 'CHAR_BIT' for the number of bits in a byte. Modified 'getBitLength' and 'getBitCount' to use the compiler's built-in intrinsics. Added comments.

--- source/library/ash.integer.bit.h
+++ source/library/ash.integer.bit.h
@@ -1,112 +1,161 @@
 #pragma once
 
 #include <cstddef>
+#include <climits>
 #include "ash.type.h"
 
 
-namespace Ash
+namespace Ash::Integer
 {
-	namespace Integer
+	/**
+	 * @brief  The size of an integer type in bits
+	 * @tparam INTEGER The integer type
+	 */
+	template
+	<
+		typename INTEGER,
+		typename = Ash::Type::IsInteger<INTEGER>
+	>
+	constexpr size_t bitSize = sizeof(INTEGER) * CHAR_BIT;
+
+	/**
+	 * @brief  Gets a bit mask for an unsigned integer value
+	 * @tparam INTEGER The unsigned integer type
+	 * @param  from    The bit position of the start of the bit mask (0 is the least significant bit)
+	 * @param  to      The bit position of the end of the bit mask (0 is the least significant bit)
+	 * @return The bit mask. This is the unsigned integer value with all the bits set between bit positions `from` and `to`.
+	 * @note   - The `from` and `to` bit positions are inclusive.
+	 * @note   - The `from` and `to` parameters are interchangable. I.e. `getBitMask(from, to) == getBitMask(to, from)`
+	 */
+	template
+	<
+		typename INTEGER,
+		typename = Ash::Type::IsUnsignedInteger<INTEGER>
+	>
+	constexpr INTEGER getBitMask(size_t from, size_t to = bitSize<INTEGER> - 1)
 	{
-		template
-		<
-			typename INTEGER,
-			typename = Ash::Type::IsInteger<INTEGER>
-		>
-		constexpr size_t getBitSize = sizeof(INTEGER) * 8;
-
-		template
-		<
-			typename INTEGER,
-			typename = Ash::Type::IsUnsignedInteger<INTEGER>
-		>
-		constexpr INTEGER getBitMask(size_t from, size_t to = getBitSize<INTEGER> - 1)
+		if (from > to)
 		{
-			if (from > to)
-			{
-				std::swap(from, to);
-			}
-
-			if (from >= getBitSize<INTEGER>)
-			{
-				return 0;
-			}
-
-			size_t size = to - from;
-			return ((size < getBitSize<INTEGER> - 1) ? (INTEGER(1) << (size + 1)) - 1 : ~INTEGER(0)) << from;
+			std::swap(from, to);
 		}
 
-		template
-		<
-			typename INTEGER,
-			typename = Ash::Type::IsUnsignedInteger<INTEGER>
-		>
-		constexpr size_t getBitLength(INTEGER value)
+		if (from >= bitSize<INTEGER>)
 		{
-			size_t size = getBitSize<INTEGER> / 2;
-
-			for (size_t n = size; n > 0; n = n / 2)
-			{
-				if ((value & getBitMask<INTEGER>(size)) == 0)
-				{
-					size = size - n / 2;
-				}
-				else
-				{
-					size = size + (n + 1) / 2;
-				}
-			}
-
-			return size;
+			return 0;
 		}
 
-		template
-		<
-			typename INTEGER,
-			typename = Ash::Type::IsUnsignedInteger<INTEGER>
-		>
-		constexpr INTEGER setBit(INTEGER value, size_t bit)
+		size_t size = to - from;
+		return ((size < bitSize<INTEGER> - 1) ? (INTEGER(1) << (size + 1)) - 1 : ~INTEGER(0)) << from;
+	}
+
+	/**
+	 * @brief  Gets the minimum number of bits that are required to hold an unsigned integer value
+	 * @tparam INTEGER The unsigned integer type
+	 * @param  value   The unsigned integer value
+	 * @return The minimum number of bits that are required to hold the `value`.
+	 * @note   - The returned value will always be at least 1. I.e. the minimum number of bits to hold a zero value is 1.
+	 */
+	template
+	<
+		typename INTEGER,
+		typename = Ash::Type::IsUnsignedInteger<INTEGER>
+	>
+	constexpr size_t getBitLength(INTEGER value)
+	{
+		if (value == 0)
 		{
-			return value | getBitMask<INTEGER>(bit, bit);
+			return 1;
 		}
-
-		template
-		<
-			typename INTEGER,
-			typename = Ash::Type::IsUnsignedInteger<INTEGER>
-		>
-		constexpr INTEGER clearBit(INTEGER value, size_t bit)
+		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned int>)
 		{
-			return value & ~getBitMask<INTEGER>(bit, bit);
+			return Ash::Integer::bitSize<unsigned int> - __builtin_clz(value);
 		}
-
-		template
-		<
-			typename INTEGER,
-			typename = Ash::Type::IsUnsignedInteger<INTEGER>
-		>
-		constexpr bool hasBitSet(INTEGER value, size_t bit)
+		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned long>)
 		{
-			return (value & getBitMask<INTEGER>(bit, bit)) != 0;
+			return Ash::Integer::bitSize<unsigned long> - __builtin_clzl(value);
 		}
-
-		template
-		<
-			typename INTEGER,
-			typename = Ash::Type::IsUnsignedInteger<INTEGER>
-		>
-		constexpr size_t getBitCount(INTEGER value)
+		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned long long>)
 		{
-			constexpr size_t bitCount[] = { 0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4 };
+			return Ash::Integer::bitSize<unsigned long long> - __builtin_clzll(value);
+		}
+	}
 
-			size_t count = 0;
-			while (value != 0)
-			{
-				count = count + bitCount[value & 0x0F];
-				value = value >> 4;
-			}
+	/**
+	 * @brief  Sets a bit in an unsigned integer value
+	 * @tparam INTEGER The unsigned integer type
+	 * @param  value   The unsigned integer value
+	 * @param  bit     The bit position to set (0 is the least significant bit)
+	 * @return The `value` with the `bit` set.
+	 */
+	template
+	<
+		typename INTEGER,
+		typename = Ash::Type::IsUnsignedInteger<INTEGER>
+	>
+	constexpr INTEGER setBit(INTEGER value, size_t bit)
+	{
+		return value | getBitMask<INTEGER>(bit, bit);
+	}
 
-			return count;
+	/**
+	 * @brief  Clears a bit in an unsigned integer value
+	 * @tparam INTEGER The unsigned integer type
+	 * @param  value   The unsigned integer value
+	 * @param  bit     The bit position to clear (0 is the least significant bit)
+	 * @return The `value` with the `bit` cleared.
+	 */
+	template
+	<
+		typename INTEGER,
+		typename = Ash::Type::IsUnsignedInteger<INTEGER>
+	>
+	constexpr INTEGER clearBit(INTEGER value, size_t bit)
+	{
+		return value & ~getBitMask<INTEGER>(bit, bit);
+	}
+
+	/**
+	 * @brief  Checks if a bit has been set in an unsigned integer value
+	 * @tparam INTEGER The unsigned integer type
+	 * @param  value   The unsigned integer value
+	 * @param  bit     The bit position to check (0 is the least significant bit)
+	 * @return `true` is returned if the `bit` in the `value` is set.
+	 * @return `false` is returned if the `bit` in the `value` is clear.
+	 */
+	template
+	<
+		typename INTEGER,
+		typename = Ash::Type::IsUnsignedInteger<INTEGER>
+	>
+	constexpr bool hasBitSet(INTEGER value, size_t bit)
+	{
+		return (value & getBitMask<INTEGER>(bit, bit)) != 0;
+	}
+
+	/**
+	 * @brief  Returns the number of bits that have been set in an unsigned integer value
+	 * @tparam INTEGER The unsigned integer type
+	 * @param  value The unsigned integer value
+	 * @return The number of bits in `value` that have been set
+	 */
+	template
+	<
+		typename INTEGER,
+		typename = Ash::Type::IsUnsignedInteger<INTEGER>
+	>
+	constexpr size_t getBitCount(INTEGER value)
+	{
+		if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned int>)
+		{
+			return __builtin_popcount(value);
+		}
+		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned long>)
+		{
+			return __builtin_popcountl(value);
+		}
+		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned long long>)
+		{
+			return __builtin_popcountll(value);
 		}
 	}
 }
