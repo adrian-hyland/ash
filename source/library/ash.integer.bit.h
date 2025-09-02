1aecc25a2383986d58ebec4e9e2b7307cd879e77
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2025-09-02 15:02:19 +0100

    Added parameter 'isSet' to 'getBitCount' to enable it to count either clear or set bits. Added functions 'getLeadingBitCount' and 'getTrailingBitCount'. Modified 'getBitLength' to use 'getLeadingBitCount'. Using fully qualified names for function calls.

--- source/library/ash.integer.bit.h
+++ source/library/ash.integer.bit.h
@@ -39,45 +39,13 @@ namespace Ash::Integer
 			std::swap(from, to);
 		}
 
-		if (from >= bitSize<INTEGER>)
+		if (from >= Ash::Integer::bitSize<INTEGER>)
 		{
 			return 0;
 		}
 
 		size_t size = to - from;
-		return ((size < bitSize<INTEGER> - 1) ? (INTEGER(1) << (size + 1)) - 1 : ~INTEGER(0)) << from;
-	}
-
-	/**
-	 * @brief  Gets the minimum number of bits that are required to hold an unsigned integer value
-	 * @tparam INTEGER The unsigned integer type
-	 * @param  value   The unsigned integer value
-	 * @return The minimum number of bits that are required to hold the `value`.
-	 * @note   - The returned value will always be at least 1. I.e. the minimum number of bits to hold a zero value is 1.
-	 */
-	template
-	<
-		typename INTEGER,
-		typename = Ash::Type::IsUnsignedInteger<INTEGER>
-	>
-	constexpr size_t getBitLength(INTEGER value)
-	{
-		if (value == 0)
-		{
-			return 1;
-		}
-		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned int>)
-		{
-			return Ash::Integer::bitSize<unsigned int> - __builtin_clz(value);
-		}
-		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned long>)
-		{
-			return Ash::Integer::bitSize<unsigned long> - __builtin_clzl(value);
-		}
-		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned long long>)
-		{
-			return Ash::Integer::bitSize<unsigned long long> - __builtin_clzll(value);
-		}
+		return ((size < Ash::Integer::bitSize<INTEGER> - 1) ? (INTEGER(1) << (size + 1)) - 1 : ~INTEGER(0)) << from;
 	}
 
 	/**
@@ -94,7 +62,7 @@ namespace Ash::Integer
 	>
 	constexpr INTEGER setBit(INTEGER value, size_t bit)
 	{
-		return value | getBitMask<INTEGER>(bit, bit);
+		return value | Ash::Integer::getBitMask<INTEGER>(bit, bit);
 	}
 
 	/**
@@ -111,7 +79,7 @@ namespace Ash::Integer
 	>
 	constexpr INTEGER clearBit(INTEGER value, size_t bit)
 	{
-		return value & ~getBitMask<INTEGER>(bit, bit);
+		return value & ~Ash::Integer::getBitMask<INTEGER>(bit, bit);
 	}
 
 	/**
@@ -129,22 +97,30 @@ namespace Ash::Integer
 	>
 	constexpr bool hasBitSet(INTEGER value, size_t bit)
 	{
-		return (value & getBitMask<INTEGER>(bit, bit)) != 0;
+		return (value & Ash::Integer::getBitMask<INTEGER>(bit, bit)) != 0;
 	}
 
 	/**
-	 * @brief  Returns the number of bits that have been set in an unsigned integer value
+	 * @brief  Returns the number of bits that are set/cleared in an unsigned integer value
 	 * @tparam INTEGER The unsigned integer type
 	 * @param  value The unsigned integer value
-	 * @return The number of bits in `value` that have been set
+	 * @param  isSet Indicates whether to count the number of bits that are set (`true`) or the number of bits that are cleared (`false`)
+	 * @return The number of bits in `value` that are set (if `isSet` is `true`).
+	 * @return The number of bits in `value` that are cleared (if `isSet` is `false`).
+	 * @note   The default value of `isSet` is `true`.
 	 */
 	template
 	<
 		typename INTEGER,
 		typename = Ash::Type::IsUnsignedInteger<INTEGER>
 	>
-	constexpr size_t getBitCount(INTEGER value)
+	constexpr size_t getBitCount(INTEGER value, bool isSet = true)
 	{
+		if (!isSet)
+		{
+			value = ~value;
+		}
+
 		if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned int>)
 		{
 			return __builtin_popcount(value);
@@ -157,5 +133,100 @@ namespace Ash::Integer
 		{
 			return __builtin_popcountll(value);
 		}
+	}
+
+	/**
+	 * @brief  Returns the number of leading (most significant) bits that are set/cleared in an unsigned integer
+	 * @tparam INTEGER The unsigned integer type
+	 * @param  value The unsigned integer value
+	 * @param  isSet Indicates whether to count the number of bits that are set (`true`) or the number of bits that are cleared (`false`)
+	 * @return The number of leading bits that are set (if `isSet` is true).
+	 * @return The number of leading bits that are cleared (if `isSet` is false).
+	 * @note   The default value of `isSet` is `false`.
+	 */
+	template
+	<
+		typename INTEGER,
+		typename = Ash::Type::IsUnsignedInteger<INTEGER>
+	>
+	constexpr size_t getLeadingBitCount(INTEGER value, bool isSet = false)
+	{
+		if (isSet)
+		{
+			value = ~value;
+		}
+
+		if (value == 0)
+		{
+			return Ash::Integer::bitSize<INTEGER>;
+		}
+		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned int>)
+		{
+			return __builtin_clz(value) + Ash::Integer::bitSize<INTEGER> - Ash::Integer::bitSize<unsigned int>;
+		}
+		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned long>)
+		{
+			return __builtin_clzl(value) + Ash::Integer::bitSize<INTEGER> - Ash::Integer::bitSize<unsigned long>;
+		}
+		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned long long>)
+		{
+			return __builtin_clzll(value) + Ash::Integer::bitSize<INTEGER> - Ash::Integer::bitSize<unsigned long long>;
+		}
+	}
+
+	/**
+	 * @brief  Returns the number of trailing (least significant) bits that are set/cleared in an unsigned integer
+	 * @tparam INTEGER The unsigned integer type
+	 * @param  value The unsigned integer value
+	 * @param  isSet Indicates whether to count the number of bits that are set (`true`) or the number of bits that are cleared (`false`)
+	 * @return The number of trailing bits that are set (if `isSet` is true).
+	 * @return The number of trailing bits that are cleared (if `isSet` is false).
+	 * @note   The default value of `isSet` is `false`.
+	 */
+	template
+	<
+		typename INTEGER,
+		typename = Ash::Type::IsUnsignedInteger<INTEGER>
+	>
+	constexpr size_t getTrailingBitCount(INTEGER value, bool isSet = false)
+	{
+		if (isSet)
+		{
+			value = ~value;
+		}
+
+		if (value == 0)
+		{
+			return Ash::Integer::bitSize<INTEGER>;
+		}
+		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned int>)
+		{
+			return __builtin_ctz(value);
+		}
+		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned long>)
+		{
+			return __builtin_ctzl(value);
+		}
+		else if constexpr (Ash::Integer::bitSize<INTEGER> <= Ash::Integer::bitSize<unsigned long long>)
+		{
+			return __builtin_ctzll(value);
+		}
+	}
+
+	/**
+	 * @brief  Gets the minimum number of bits that are required to hold an unsigned integer value
+	 * @tparam INTEGER The unsigned integer type
+	 * @param  value   The unsigned integer value
+	 * @return The minimum number of bits that are required to hold the `value`.
+	 * @note   - The returned value will always be at least 1. I.e. the minimum number of bits to hold a zero value is 1.
+	 */
+	template
+	<
+		typename INTEGER,
+		typename = Ash::Type::IsUnsignedInteger<INTEGER>
+	>
+	constexpr size_t getBitLength(INTEGER value)
+	{
+		return (value != 0) ? Ash::Integer::bitSize<INTEGER> - Ash::Integer::getLeadingBitCount(value) : 1;
 	}
 }
