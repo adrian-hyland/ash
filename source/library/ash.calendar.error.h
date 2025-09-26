75b51eb6670ebb29f4b21c9d14cdeaba98530841
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2025-09-26 12:16:03 +0100

    Added 'invalidHour', 'invalidMinute' and 'invalidSecond' error values. Renamed 'invalidYearMonthDay' to 'invalidDay', 'invalidYearOrdinal' to 'invalidOrdinal' and 'invalidYearWeek' to 'invalidWeek'.

--- source/library/ash.calendar.error.h
+++ source/library/ash.calendar.error.h
@@ -29,13 +29,16 @@ namespace Ash
 
 			static inline const Category category;
 
-			static constexpr Ash::Error::Value invalidDuration     = Ash::Error::Value(category, 0);
-			static constexpr Ash::Error::Value invalidYear         = Ash::Error::Value(category, 1);
-			static constexpr Ash::Error::Value invalidMonth        = Ash::Error::Value(category, 2);
-			static constexpr Ash::Error::Value invalidWeekday      = Ash::Error::Value(category, 3);
-			static constexpr Ash::Error::Value invalidYearMonthDay = Ash::Error::Value(category, 4);
-			static constexpr Ash::Error::Value invalidYearOrdinal  = Ash::Error::Value(category, 5);
-			static constexpr Ash::Error::Value invalidYearWeek     = Ash::Error::Value(category, 6);
+			static constexpr Ash::Error::Value invalidDuration = Ash::Error::Value(category, 0);
+			static constexpr Ash::Error::Value invalidYear     = Ash::Error::Value(category, 1);
+			static constexpr Ash::Error::Value invalidMonth    = Ash::Error::Value(category, 2);
+			static constexpr Ash::Error::Value invalidWeekday  = Ash::Error::Value(category, 3);
+			static constexpr Ash::Error::Value invalidDay      = Ash::Error::Value(category, 4);
+			static constexpr Ash::Error::Value invalidOrdinal  = Ash::Error::Value(category, 5);
+			static constexpr Ash::Error::Value invalidWeek     = Ash::Error::Value(category, 6);
+			static constexpr Ash::Error::Value invalidHour     = Ash::Error::Value(category, 7);
+			static constexpr Ash::Error::Value invalidMinute   = Ash::Error::Value(category, 8);
+			static constexpr Ash::Error::Value invalidSecond   = Ash::Error::Value(category, 9);
 
 		protected:
 			static inline bool getErrorCodeDescription(Code code, Ash::Error::Code::Description description)
@@ -46,9 +49,12 @@ namespace Ash
 					"Invalid year value",
 					"Invalid month value",
 					"Invalid weekday value",
-					"Invalid day value for the month and year that was given",
-					"Invalid ordinal value for the year that was given",
-					"Invalid week value for the year that was given"
+					"Invalid day value",
+					"Invalid ordinal value",
+					"Invalid week value",
+					"Invalid hour value",
+					"Invalid minute value",
+					"Invalid second value"
 				};
 				const char *errorDescription = (code < sizeof(errorDescriptions) / sizeof(errorDescriptions[0])) ? errorDescriptions[code] : nullptr;
 				if ((errorDescription == nullptr) || (strlen(errorDescription) > sizeof(Ash::Error::Code::Description) - 1))
