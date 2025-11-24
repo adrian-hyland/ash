4874a94f58d5fdedc995631c216e9666516ed37c
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2025-11-24 10:44:08 +0000

    Added methods 'construct', 'destroy', 'defaultConstruct', 'copyConstruct' and 'defaultConstruct' for managing the construction and destruction of objects in place.

--- source/library/ash.memory.core.h
+++ source/library/ash.memory.core.h
@@ -155,6 +155,160 @@ namespace Ash
 
 		template
 		<
+			typename TYPE,
+			typename ...ARGUMENT
+		>
+		constexpr void construct(TYPE &value, ARGUMENT &&...argument)
+		{
+			if constexpr (Ash::Type::isArray<TYPE>)
+			{
+				typename Ash::Type::Array<TYPE>::BaseType elementValue(std::forward<ARGUMENT>(argument)...);
+				for (size_t n : Ash::Iterate<size_t>::from(0, Ash::Type::Array<TYPE>::size))
+				{
+					construct(value[n], elementValue);
+				}
+			}
+			else
+			{
+				new (&value) TYPE(std::forward<ARGUMENT>(argument)...);
+			}
+		}
+
+		template
+		<
+			typename TYPE
+		>
+		constexpr void destroy(TYPE &value)
+		{
+			if constexpr (Ash::Type::hasDestructor<TYPE>)
+			{
+				if constexpr (Ash::Type::isArray<TYPE>)
+				{
+					for (size_t n : Ash::Iterate<size_t>::from(0, Ash::Type::Array<TYPE>::size))
+					{
+						destroy(value[n]);
+					}
+				}
+				else
+				{
+					value.~TYPE();
+				}
+			}
+		}
+
+		template
+		<
+			typename TYPE
+		>
+		constexpr void destroy(TYPE *content, size_t length)
+		{
+			if constexpr (Ash::Type::hasDestructor<TYPE>)
+			{
+				for (size_t n : Ash::Iterate<size_t>::from(0, length))
+				{
+					destroy(content[n]);
+				}
+			}
+		}
+
+		template
+		<
+			typename TYPE
+		>
+		constexpr size_t defaultConstruct(TYPE *content, size_t length)
+		{
+			if constexpr (Ash::Type::isArray<TYPE>)
+			{
+				for (size_t n : Ash::Iterate<size_t>::from(0, length))
+				{
+					defaultConstruct(content[n], Ash::Type::Array<TYPE>::size);
+				}
+			}
+			else
+			{
+				if constexpr (Ash::Type::isPrimitive<TYPE>)
+				{
+					if (!std::is_constant_evaluated() && (length > sizeof(uint64_t) / sizeof(TYPE)))
+					{
+						::memset(content, 0, length * sizeof(TYPE));
+						return length;
+					}
+				}
+
+				for (size_t n : Ash::Iterate<size_t>::from(0, length))
+				{
+					construct(content[n], TYPE());
+				}
+			}
+			return length;
+		}
+
+		template
+		<
+			typename TYPE
+		>
+		constexpr size_t copyConstruct(TYPE *to, const TYPE *from, size_t length)
+		{
+			if constexpr (Ash::Type::isArray<TYPE>)
+			{
+				for (size_t n : Ash::Iterate<size_t>::from(0, length))
+				{
+					copyConstruct(to[n], from[n], Ash::Type::Array<TYPE>::size);
+				}
+			}
+			else
+			{
+				if constexpr (Ash::Type::isPrimitive<TYPE>)
+				{
+					if (!std::is_constant_evaluated() && (length > sizeof(uint64_t) / sizeof(TYPE)))
+					{
+						::memmove(to, from, length * sizeof(TYPE));
+						return length;
+					}
+				}
+
+				for (size_t n : Ash::Iterate<size_t>::from(0, length))
+				{
+					construct(to[n], from[n]);
+				}
+			}
+			return length;
+		}
+
+		template
+		<
+			typename TYPE
+		>
+		constexpr size_t moveConstruct(TYPE *to, TYPE *from, size_t length)
+		{
+			if constexpr (Ash::Type::isArray<TYPE>)
+			{
+				for (size_t n : Ash::Iterate<size_t>::from(0, length))
+				{
+					moveConstruct(to[n], from[n], Ash::Type::Array<TYPE>::size);
+				}
+			}
+			else
+			{
+				if constexpr (Ash::Type::isPrimitive<TYPE>)
+				{
+					if (!std::is_constant_evaluated() && (length > sizeof(uint64_t) / sizeof(TYPE)))
+					{
+						::memmove(to, from, length * sizeof(TYPE));
+						return length;
+					}
+				}
+
+				for (size_t n : Ash::Iterate<size_t>::from(0, length))
+				{
+					construct(to[n], std::move(from[n]));
+				}
+			}
+			return length;
+		}
+
+		template
+		<
 			typename TYPE
 		>
 		constexpr size_t clear(TYPE *content, size_t length)
