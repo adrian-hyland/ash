090a87b0563e21f693039652f53ba32a68e9f861
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2025-12-08 13:39:35 +0000

    Removed the 'replacement' unicode character (from 'Ash::Encoding::Utf16le::Character'). Modified the 'Ash::Unicode::Character' type conversion operator (in Ash::Encoding::Utf16le::Character) to return a null character if there is no encoded character. Modified the 'set' methods (that set the character to one or more code units in 'Ash::Encoding::Utf16le::Character') to return an 'Ash::Error::Value'. Modified the 'decodeNext' and 'decodePrevious' methods (in 'Ash::Encoding::Utf16le') to return an 'Ash::Error::Value'. Added helper methods 'getNextCode' and 'getPreviousCode' to 'Ash::Encoding::Utf16le'.

--- source/library/ash.encoding.utf16le.h
+++ source/library/ash.encoding.utf16le.h
@@ -24,8 +24,6 @@ namespace Ash
 			class Character : public Ash::Memory::Buffer<Code, maxSize>
 			{
 			public:
-				static constexpr Ash::Unicode::Character replacement = Ash::Unicode::Character::replacement;
-
 				constexpr Character() : Ash::Memory::Buffer<Code, maxSize>() {}
 
 				constexpr Character(Ash::Unicode::Character character) : Ash::Memory::Buffer<Code, maxSize>()
@@ -41,17 +39,23 @@ namespace Ash
 
 				constexpr operator Ash::Unicode::Character () const
 				{
-					if (getLength() == 2)
+					switch (getLength())
 					{
-						return (getCode(1) << 8) + getCode(0);
-					}
-					else if (getLength() == 4)
-					{
-						return 0x10000 + ((getCode(1) & 0x03) << 18) + (getCode(0) << 10) + ((getCode(3) & 0x03) << 8) + getCode(2);
-					}
-					else
-					{
-						return Ash::Unicode::Character::replacement;
+						case 0:
+							return '\0';
+						break;
+
+						case 2:
+							return (getCode(1) << 8) + getCode(0);
+						break;
+
+						case 4:
+							return 0x10000 + ((getCode(1) & 0x03) << 18) + (getCode(0) << 10) + ((getCode(3) & 0x03) << 8) + getCode(2);
+						break;
+
+						default:
+							return Ash::Unicode::Character::replacement;
+						break;
 					}
 				}
 
@@ -64,14 +68,14 @@ namespace Ash
 
 					if (value < 0x10000)
 					{
-						setLength(2);
+						setLength(minSize).assertErrorNotSet();
 						(*this)[0] = value & 0xFF;
 						(*this)[1] = value >> 8;
 					}
 					else
 					{
 						value = value - 0x10000;
-						setLength(4);
+						setLength(maxSize).assertErrorNotSet();
 						(*this)[0] = value >> 10;
 						(*this)[1] = ((value >> 18) & 0x03) | 0xD8;
 						(*this)[2] = value;
@@ -79,83 +83,136 @@ namespace Ash
 					}
 				}
 
-				constexpr size_t set(Code code1, Code code2)
+				[[nodiscard]]
+				constexpr Ash::Error::Value set(Code code1, Code code2)
 				{
-					setLength(2);
+					setLength(minSize).assertErrorNotSet();
 					(*this)[0] = code1;
 					(*this)[1] = code2;
-					return 2;
+					return Ash::Error::none;
 				}
 
-				constexpr size_t set(Code code1, Code code2, Code code3, Code code4)
+				[[nodiscard]]
+				constexpr Ash::Error::Value set(Code code1, Code code2, Code code3, Code code4)
 				{
-					setLength(4);
+					setLength(maxSize).assertErrorNotSet();
 					(*this)[0] = code1;
 					(*this)[1] = code2;
 					(*this)[2] = code3;
 					(*this)[3] = code4;
-					return 4;
+					return Ash::Error::none;
 				}
 
 				friend Utf16le;
 			};
 
-			static constexpr size_t decodeNext(Ash::Memory::View<Code> value, size_t offset, Character &character)
+			[[nodiscard]]
+			static constexpr Ash::Error::Value decodeNext(Ash::Memory::View<Code> value, size_t offset, Character &character)
 			{
 				Code code1 = 0;
 				Code code2 = 0;
 				Code code3 = 0;
 				Code code4 = 0;
 
-				if (value.get(offset++, code1) && value.get(offset++, code2))
+				Ash::Error::Value error = getNextCode(value, offset, code1);
+				if (!error.hasErrorSet())
 				{
-					if ((code2 & 0xFC) == 0xD8)
+					error = getNextCode(value, offset, code2);
+					if (!error.hasErrorSet())
 					{
-						if (value.get(offset++, code3) && value.get(offset++, code4))
+						if ((code2 & 0xFC) == 0xD8)
 						{
-							if ((code4 & 0xFC) == 0xDC)
+							error = getNextCode(value, offset, code3);
+							if (!error.hasErrorSet())
 							{
-								return character.set(code1, code2, code3, code4);
+								error = getNextCode(value, offset, code4);
+								if (!error.hasErrorSet())
+								{
+									if ((code4 & 0xFC) == 0xDC)
+									{
+										return character.set(code1, code2, code3, code4);
+									}
+									else
+									{
+										error = Ash::Encoding::Error::invalidCodeUnit;
+									}
+								}
 							}
 						}
-					}
-					else if ((code2 & 0xFC) != 0xDC)
-					{
-						return character.set(code1, code2);
+						else if ((code2 & 0xFC) != 0xDC)
+						{
+							return character.set(code1, code2);
+						}
+						else
+						{
+							error = Ash::Encoding::Error::invalidCodeUnit;
+						}
 					}
 				}
 
 				character.clear();
-				return 0;
+				return error;
 			}
 
-			static constexpr size_t decodePrevious(Ash::Memory::View<Code> value, size_t offset, Character &character)
+			[[nodiscard]]
+			static constexpr Ash::Error::Value decodePrevious(Ash::Memory::View<Code> value, size_t offset, Character &character)
 			{
 				Code code1 = 0;
 				Code code2 = 0;
 				Code code3 = 0;
 				Code code4 = 0;
 
-				if ((offset > 1) && value.get(--offset, code1) && value.get(--offset, code2))
+				Ash::Error::Value error = getPreviousCode(value, offset, code1);
+				if (!error.hasErrorSet())
 				{
-					if ((code1 & 0xFC) == 0xDC)
+					error = getPreviousCode(value, offset, code2);
+					if (!error.hasErrorSet())
 					{
-						if ((offset > 1) && value.get(--offset, code3) && value.get(--offset, code4))
+						if ((code1 & 0xFC) == 0xDC)
 						{
-							if ((code3 & 0xFC) == 0xD8)
+							error = getPreviousCode(value, offset, code3);
+							if (!error.hasErrorSet())
 							{
-								return character.set(code4, code3, code2, code1);
+								error = getPreviousCode(value, offset, code4);
+								if (!error.hasErrorSet())
+								{
+									if ((code3 & 0xFC) == 0xD8)
+									{
+										return character.set(code4, code3, code2, code1);
+									}
+									else
+									{
+										error = Ash::Encoding::Error::invalidCodeUnit;
+									}
+								}
 							}
 						}
-					}
-					else if ((code1 & 0xFC) != 0xD8)
-					{
-						return character.set(code2, code1);
+						else if ((code1 & 0xFC) != 0xD8)
+						{
+							return character.set(code2, code1);
+						}
+						else
+						{
+							error = Ash::Encoding::Error::invalidCodeUnit;
+						}
 					}
 				}
 
 				character.clear();
-				return 0;
+				return error;
+			}
+
+		protected:
+			[[nodiscard]]
+			static constexpr Ash::Error::Value getNextCode(Ash::Memory::View<Code> value, size_t &offset, Code &code)
+			{
+				return value.get(offset++, code);
+			}
+
+			[[nodiscard]]
+			static constexpr Ash::Error::Value getPreviousCode(Ash::Memory::View<Code> value, size_t &offset, Code &code)
+			{
+				return (offset != 0) ? value.get(--offset, code) : Ash::Memory::Error::readAccessOutOfBound;
 			}
 
 		private:
