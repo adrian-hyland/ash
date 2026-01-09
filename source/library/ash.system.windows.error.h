10f3133ce05da6ed82b4c9810839a403080d3fec
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2026-01-09 19:22:19 +0000

    Converted 'Ash::System::Windows::Error' to a namespace. Moved 'getErrorDescription' to 'Ash::System::Windows::Error::Category' (and renamed it to 'getDescription'). Added 'pathNotFound', 'accessDenied', 'fileNotOpen', 'diskOutOfSpace' and 'environmentNameNotFound' error values. Added 'Ash::System::Windows::error' method.

--- source/library/ash.system.windows.error.h
+++ source/library/ash.system.windows.error.h
@@ -10,9 +10,8 @@ namespace Ash
 	{
 		namespace Windows
 		{
-			class Error : public Ash::Error::Value
+			namespace Error
 			{
-			public:
 				using Code = DWORD;
 
 				class Category : public Ash::Error::Category
@@ -25,28 +24,29 @@ namespace Ash
 
 					virtual bool getErrorCodeDescription(Ash::Error::Code code, Ash::Error::Code::Description description) const override
 					{
-						return Ash::System::Windows::Error::getErrorCodeDescription(code, description);
+						return getDescription(code, description);
+					}
+
+				protected:
+					static bool getDescription(Code code, Ash::Error::Code::Description description)
+					{
+						return ::FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), description, sizeof(Ash::Error::Code::Description) - 1, nullptr) > 0;
 					}
 				};
 
-				static inline const Category category;
+				inline constexpr Category category;
 
-				Error() : Error(GetLastError()) {}
+				constexpr Ash::Error::Value pathNotFound            = { category, ERROR_PATH_NOT_FOUND   };
+				constexpr Ash::Error::Value accessDenied            = { category, ERROR_ACCESS_DENIED    };
+				constexpr Ash::Error::Value fileNotOpen             = { category, ERROR_INVALID_HANDLE   };
+				constexpr Ash::Error::Value diskOutOfSpace          = { category, ERROR_DISK_FULL        };
+				constexpr Ash::Error::Value environmentNameNotFound = { category, ERROR_ENVVAR_NOT_FOUND };
+			}
 
-				Error(Code code) : Ash::Error::Value()
-				{
-					if (code != ERROR_SUCCESS)
-					{
-						set(category, code);
-					}
-				}
-
-			protected:
-				static bool getErrorCodeDescription(Code code, Ash::Error::Code::Description description)
-				{
-					return ::FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, nullptr, code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), description, sizeof(Ash::Error::Code::Description) - 1, nullptr) > 0;
-				}
-			};
+			constexpr Ash::Error::Value error(Ash::System::Windows::Error::Code code = ::GetLastError())
+			{
+				return (code == ERROR_SUCCESS) ? Ash::Error::none : Ash::Error::Value(Ash::System::Windows::Error::category, code);
+			}
 		}
 	}
 }
