a1c652b95fdb3a9aac08787e3761d3cb0fc1127b
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2026-02-20 14:56:18 +0000

    Initial check in

---
+++ source/library/ash.system.windows.ui.error.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <cstring>
+#include "ash.system.windows.error.h"
+
+
+namespace Ash
+{
+	namespace System
+	{
+		namespace Windows
+		{
+			namespace UI
+			{
+				namespace Error
+				{
+					using Code = unsigned int;
+
+					class Category : public Ash::Error::Category
+					{
+					public:
+						virtual Name getName() const override
+						{
+							return "Ash::System::Windows::UI::Error";
+						}
+
+						virtual bool getErrorCodeDescription(Ash::Error::Code code, Ash::Error::Code::Description description) const override
+						{
+							return getDescription(code, description);
+						}
+
+					protected:
+						static bool getDescription(Code code, Ash::Error::Code::Description description)
+						{
+							static const char *errorDescriptions[] =
+							{
+								"Exit event loop signalled",
+								"Invalid window",
+								"No display settings",
+								"No monitor settings",
+							};
+
+							const char *errorDescription = (code < sizeof(errorDescriptions) / sizeof(errorDescriptions[0])) ? errorDescriptions[code] : nullptr;
+							if ((errorDescription == nullptr) || (strlen(errorDescription) > sizeof(Ash::Error::Code::Description) - 1))
+							{
+								return false;
+							}
+
+							strcpy(description, errorDescription);
+							return true;
+						}
+					};
+
+					inline constexpr Category category;
+
+					constexpr Ash::Error::Value exitEventLoop     = { category, 0 };
+					constexpr Ash::Error::Value invalidWindow     = { category, 1 };
+					constexpr Ash::Error::Value noDisplaySettings = { category, 2 };
+					constexpr Ash::Error::Value noMonitorSettings = { category, 3 };
+				}
+			}
+		}
+	}
+}
