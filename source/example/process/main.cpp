dd3f82a7429f9e347b312b8b832a1c9ba103969d
Adrian Hyland <60901736+adrian-hyland@users.noreply.github.com>
2026-02-21 18:47:08 +0000

    Getting the value of a dummy 'UNKNOWN' environment variable and adding it to the environment block of the new process (to check that nothing is added or passed acroess). Now throwing an exception if any error occurs.

--- source/example/process/main.cpp
+++ source/example/process/main.cpp
@@ -29,18 +29,17 @@ int main(int argc, char *argv[], char *envp[])
 			{
 				Ash::Concurrency::Process::Environment::get("PATH"),
 				Ash::Concurrency::Process::Environment::get("TMP"),
+				Ash::Concurrency::Process::Environment::get("UNKNOWN"),
 				{ "test1", "value1" },
 			}
 		);
-		block.set({ "test2", "hello world" });
+
+		block.set({ "test2", "hello world" }).throwOnError();
 
 		std::cout << "\nSelf replicating...\n";
 		Ash::Concurrency::Process process;
 		// Variety of different command line arguments to check that they get passed as given when running in a Windows environment...
-		if (!process.run({ Ash::Concurrency::Process::getCurrentName(), "argument 1", "I said \"hello world!\"", "argument\"3", "test\\path with\\spaces\\", "\\\"argument\\5\\" }, block))
-		{
-			std::cout << "Failed to start process\n";
-		}
+		process.run({ Ash::Concurrency::Process::getCurrentName(), "argument 1", "I said \"hello world!\"", "argument\"3", "test\\path with\\spaces\\", "\\\"argument\\5\\" }, block).throwOnError();
 	}
 
 	return 0;
